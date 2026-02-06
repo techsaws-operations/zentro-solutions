08f0486a79421488b14469d5d93fae84d72035f5
syeddhassannaali <syeddhassannaali@gmail.com>
2026-02-06 08:52:51 +0500

    Fixed vercel errors

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,6 +1,6 @@
 import { NewsletterSection } from "@/components/partials/newsletter-section";
 import { ReviewsSection } from "@/components/partials/reviews-section";
-import { FeatureSection, PlanSection } from "@/components/sections/pricing/feature-section";
+import { FeatureSection } from "@/components/sections/pricing/feature-section";
 import { PricingSection } from "@/components/sections/pricing/pricing-section";
 import { PricingComparison } from "@/components/sections/pricing/pricingcompare";
 import { PricingQASection } from "@/components/sections/pricing/pricingqa-section";
